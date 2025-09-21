3b7ae0555024975a7cadfdc20bff85e47f8f338b
SUNNY-dev009 <sunnysendemails@gmail.com>
2025-09-21 12:27:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("new changes applied");
+  res.send("back to old change ");
 });
 
 app.listen(4000, () => {
