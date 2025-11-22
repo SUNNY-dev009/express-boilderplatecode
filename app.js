a00063f40aec0261262dd4d028730aa835294761
SUNNY-dev009 <sunnysendemails@gmail.com>
2025-11-22 14:34:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
   res.send("chaning change.. ");
 });
 
+app.get("/test", (req, res) => {
+  res.send("added logs.. ");
+  console.log("Adding logs to check...")
+});
+
 app.listen(4000, () => {
   console.log("server started");
 });
